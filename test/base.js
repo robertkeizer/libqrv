3ad57e87e1d76a1a4d97047c7ee412b206c8dab5
Robert Keizer <robert@keizer.ca>
2019-11-11 12:05:53 -0600

    Validation and some initial bits in the main libqrv.

--- test/base.js
+++ test/base.js
@@ -3,7 +3,20 @@ const LibQRV = require( "../lib/index" );
 const assert = require( "assert" );
 
 describe( "Base", function( ){
-	it.only( "LibQRV is a function", function( ){
+	it( "LibQRV is a function", function( ){
 		assert.ok( typeof( LibQRV ) == "function" );
+	} );
+
+	describe( "Validations", function( ){
+		it( "Fails if an invalid config is passed in", function( cb ){
+			const z = new LibQRV( { "foo": "bar" }, ( err ) => {
+				if( !err ){ return cb( "No error" ); }
+				return cb( null );
+			} );
+		} );
+	} );
+
+	it.only( "Constructor returns sane object in callback", function( cb ){
+		
 	} );
 } );
